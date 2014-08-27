eb3f01ba53054d9cb5b91c12d8a4187dc2e13b57
Jacob <jacob@Jacobs-MacBook-Pro.local>
2014-08-28 02:35:44 +0300

    saved changes

---
+++ js/fontSizer.js
@@ -0,0 +1,17 @@
+//$(document).ready(function(){
+//    var fontSize;
+//    var classList = $('.chart');
+//    var idName;
+//    var myId;
+//    var cnt;
+//    $.each(classList, function(index, item){
+//            myId = "#skillz #" + item.id + " .skill-text";
+//            fontSize = $(myId).css('font-size');
+//            cnt = 10;
+//            while($(myId).width() > 62 || cnt > 0) {
+//               $(myId).css('font-size', fontSize - 1);
+//                fontSize = fontSize - 1;
+//                cnt--;
+//            }
+//    });
+//});
